f1e711c8080ced066524b5be1c07a7897899eb61
ABHIRAJ VERMA <abhirajverma0133@gmail.com>
2026-07-09 23:21:16 +0530

    Time: 3 ms (40.68%) | Memory: 15.9 MB (65.88%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -6,40 +6,57 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>>nodes;
-        queue<pair<TreeNode*,pair<int,int>>>todo;
-        todo.push({root,{0,0}});
 
-        while(!todo.empty()){
+        // x -> vertical level
+        // y -> depth
+        // multiset -> keeps values sorted if same (x,y)
+        map<int, map<int, multiset<int>>> nodes;
+
+        queue<pair<TreeNode*, pair<int, int>>> todo;
+
+        todo.push({root, {0, 0}});
+
+        while (!todo.empty()) {
+
             auto p = todo.front();
             todo.pop();
+
             TreeNode* node = p.first;
-            int x= p.second.first ;
-            int y = p.second.second ;
+            int x = p.second.first;
+            int y = p.second.second;
+
             nodes[x][y].insert(node->val);
-            
-            if(node->left){
-                todo.push({node->left,{x-1,y+1}});
+
+            if (node->left) {
+                todo.push({node->left, {x - 1, y + 1}});
             }
-            if(node->right){
-                todo.push({node->right,{x+1,y+1}});
+
+            if (node->right) {
+                todo.push({node->right, {x + 1, y + 1}});
             }
         }
 
-        vector<vector<int>>ans;
-        for(auto p:nodes){
-            vector<int>col;
-            for(auto q:p.second){
-                col.insert(col.end(),q.second.begin(),q.second.end());
+        vector<vector<int>> ans;
+
+        for (auto &p : nodes) {
+
+            vector<int> col;
+
+            for (auto &q : p.second) {
+                col.insert(col.end(), q.second.begin(), q.second.end());
             }
+
             ans.push_back(col);
         }
+
         return ans;
     }
 };
