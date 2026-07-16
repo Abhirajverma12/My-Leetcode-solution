debc82a04473f205c07fd26ca8c07496b0ea8dea
ABHIRAJ VERMA <abhirajverma0133@gmail.com>
2026-07-16 18:27:11 +0530

    Time: 66 ms (33.48%) | Memory: 44.1 MB (20.31%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,17 +9,23 @@
  */
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(p == root || q == root) return root;
+// TC - O(N) , SC - O(1) 
+// without any extra space 
 
-        if(root == NULL) return nullptr;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL) return nullptr; // basecase 
+
+        if(p == root || q == root) return root; // if any of p or q is root than we have to return root 
 
         TreeNode* left = lowestCommonAncestor(root->left,p,q);
         TreeNode* right = lowestCommonAncestor(root->right,p,q);
-        
-        if(left && right) return root;
-        if(left)return left;
-        if(right)return right;
-        return nullptr;
+
+        if(left && right) return root; // if from both left & right side p , q is found so we have to return that current root 
+
+        if(left)return left; // if any of p,q is found at the left so we return left
+
+        if(right)return right;// if any of p,q is found at right so we return right
+
+        return nullptr; // if any of p,q is not found at the left or at the right so we have to return the 'nullptr'
     }
 };
