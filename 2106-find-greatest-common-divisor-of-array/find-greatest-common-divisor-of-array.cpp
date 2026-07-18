ce8560a5adc99947c17aec06f0420e512fff25eb
ABHIRAJ VERMA <abhirajverma0133@gmail.com>
2026-07-19 01:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (31.47%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
@@ -4,9 +4,9 @@ public:
         int maxnum = *max_element(nums.begin(),nums.end());
         int minnum = *min_element(nums.begin(),nums.end());
         int ans=1;
-        for(int i=2;i<=maxnum;i++){
+        for(int i=2;i<=minnum;i++){
             if((maxnum%i == 0 ) && (minnum%i == 0)){
-                ans =i;
+                ans = i;
             }
         }
         return ans;
