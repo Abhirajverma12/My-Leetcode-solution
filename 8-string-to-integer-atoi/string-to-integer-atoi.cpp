b4204bbe0e69247ab07e8a4984f58c8f8a9dcc2f
ABHIRAJ VERMA <abhirajverma0133@gmail.com>
2026-08-20 01:27:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (54.92%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
+
+    long long convert(string &s, int i, long long ans, int sign) {
+
+        // Base case
+        if (i >= s.size() || !isdigit(s[i])) {
+            return ans * sign;
+        }
+
+        int digit = s[i] - '0';
+
+        // Overflow check
+        if (sign == 1 && ans > (INT_MAX - digit) / 10)
+            return INT_MAX;
+
+        if (sign == -1 && ans > (-(long long)INT_MIN - digit) / 10)
+            return INT_MIN;
+
+        ans = ans * 10 + digit;
+
+        return convert(s, i + 1, ans, sign);
+    }
+
     int myAtoi(string s) {
-        int n = s.size();
-        int cnt = 0 , sign = 1 ;
-        long long ans =0;
-        int i=0;
-        while( i < n && s[i] == ' '){i++;}
-        
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-') {
+
+        int i = 0;
+
+        // 1. Skip spaces
+        while (i < s.size() && s[i] == ' ')
+            i++;
+
+        // 2. Sign
+        int sign = 1;
+
+        if (i < s.size() && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
                 sign = -1;
-            }
+
             i++;
         }
 
-        while(i<n &&   isdigit(s[i]) ){
-            int temp = s[i]-'0';
-
-            if (ans > (INT_MAX - temp) / 10) {
-                if (sign == 1)
-                    return INT_MAX;
-                else
-                    return INT_MIN;
-            }
-            ans = ans*10 + temp;
-            i++;
-        }
-        return ans*sign;
+        // 3. Recursively process digits
+        return convert(s, i, 0, sign);
     }
 };
