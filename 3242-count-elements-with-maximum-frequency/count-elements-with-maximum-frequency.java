fcda3a9e1dce1f255ee514465f779be31e9cef70
ABHIRAJ VERMA <abhirajverma0133@gmail.com>
2026-08-22 23:20:38 +0530

    Time: 3 ms (29.84%) | Memory: 44 MB (68.48%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -1,34 +1,30 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        // we intialize the HashMap in java as alternate of the unordered_map in c++ 
+        // use "var" rather than "HashMap<Integer,Integer> entry" while traverse in the hashmap while traversing 
         HashMap<Integer,Integer>freq = new HashMap<>();
-         
-         // in java we count the freq of each character and now we if already exists than we increase count and not exists so simply put 1 count to it 
+
         for(int i:nums){
             if(freq.containsKey(i)){
                 freq.put(i,freq.get(i)+1);
-            }
-            else{
+            }else{
                 freq.put(i,1);
             }
         }
-        int maxv = Integer.MIN_VALUE;
 
-        // here we find the max frequency ..
-        for(int value:freq.values()){
-            maxv = Math.max(value,maxv);
+        int maxv = 0;
+        for(var entry : freq.entrySet()){
+            int key = entry.getKey();
+            int value = entry.getValue();
+            maxv = Math.max(maxv,value);
         }
-
-        int sum= 0;
-        for(Map.Entry<Integer,Integer> entry : freq.entrySet()){
+        int ans =0;
+        for(var entry : freq.entrySet()){
             int key = entry.getKey();
             int value = entry.getValue();
             if(value == maxv){
-                sum += value;
+                ans += value;
             }
         }
-
-        return sum ;
-
+        return ans;
     }
 }
